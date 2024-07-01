09ccb0684449a1291b26094889a4db8427776fab
milenabeicker <milenakessia87@gmail.com>
2024-07-01 12:21:33 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Milena Beicker on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
